35e9da4f9e2aacfda7e6c09e7223e6e32378e465
Guicortiz <guilhermechenciortiz@gmail.com>
2021-11-08 22:00:21 -0300

    Testing career and career list, and order of itens

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Curse.ContentContext;
 
 namespace Curse
@@ -20,6 +21,35 @@ namespace Curse
                 Console.WriteLine(article.Title);
                 Console.WriteLine(article.Url);
             }
+
+            var courses = new List<Course>();
+            var coursesOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var coursesCsharp = new Course("Fundamentos C#", "fundamentos-C#");
+            var coursesdotnet = new Course("Fundamentos .net", "fundamentos-.net");
+
+            courses.Add(coursesOOP);
+            courses.Add(coursesCsharp);
+            courses.Add(coursesdotnet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialist .Net", "especialist-dotnet");
+            var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+            var careerItem2 = new CareerItem(2, "Aprenda dotnet", "", null);
+            var careerItem3 = new CareerItem(3, "Aprenda OOP", "", null);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem3);
+            careerDotnet.Items.Add(careerItem);
+            careers.Add(careerDotnet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
+            }
+
         }
     }
 }
